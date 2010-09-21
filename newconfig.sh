932b35cb070ab8c4f0540e8f075a7f99c10b982f
Joel Barciauskas <jbarciauskas@bluestatedigital.com>
2010-09-21 16:21:36 -0400

    fix newconfig.sh

--- newconfig.sh
+++ newconfig.sh
@@ -2,12 +2,12 @@ cd ~
 #Backup your local configuration files, example:
 mv .bash_profile .bash_profile.bk
 #Clone your configuration repository
-git clone git@github.com:GITHUB_USERNAME/config.git config.git
+git clone git@github.com:jbarciauskas/config.git config.git
 #Move the git metadata to ~/.config.git
 mv config.git/.git .config.git
 #Enable dotglob
 shopt -s dotglob
 #Move your configuration files to your home directory
-mv -i config.git/* .
+cp -R -i config.git/* .
 #Delete the config.git directory
 rmdir config.git
